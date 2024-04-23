2fd21ef9e18c3fd3d194f30de1d6518ae39339d5
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-04-23 20:56:03 +0100

    test newly written functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
+#include <Eigen/Dense>
+
 #include "../include/dataset_utils.h"
+
 
 int main() {
 
-    // Define the number of data items
-    const int ITEM_COUNT = 10;
+    std::string filename = "../data/train-images-idx3-ubyte";
 
-    // Generate randomized indices
-    std::vector<int> indices = generateRandomIndices(ITEM_COUNT);
+    Eigen::MatrixXf mnistData = readData(filename);
 
-    // Print the randomized indices
-    std::cout << "Randomized indices:" << std::endl;
-    for (int idx : indices) {
-        std::cout << idx << " ";
+    std::cout << "Loaded MNIST data from " << filename << std::endl;
+    std::cout << "Number of images: " << mnistData.cols() << std::endl;
+    std::cout << "Image size: " << mnistData.rows() << "x1 (flattened)" << std::endl;
+
+    std::cout << "Pixel values of the first flattened image:" << std::endl;
+    for (int i = 0; i < mnistData.rows(); ++i) {
+        std::cout << mnistData(i, 4) << " ";
     }
     std::cout << std::endl;
 
+
     return 0;
 }
-
