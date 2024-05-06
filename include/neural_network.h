261978cb20e5cd75bbb1970ddedf04652679e973
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-05-06 16:07:07 +0100

    update header file

--- include/neural_network.h
+++ include/neural_network.h
@@ -5,4 +5,7 @@
 
 std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> forwardPropagation(Eigen::MatrixXf W1, Eigen::MatrixXf b1, Eigen::MatrixXf W2, Eigen::MatrixXf b2, Eigen::MatrixXf X);
 
+std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> backwardPropagation(Eigen::MatrixXf Z1, Eigen::MatrixXf A1, Eigen::MatrixXf Z2, Eigen::MatrixXf A2,Eigen::MatrixXf W1, Eigen::MatrixXf W2, Eigen::MatrixXf X, Eigen::VectorXi Y);
+
+std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> updateParameters(Eigen::MatrixXf W1,Eigen::MatrixXf b1,Eigen::MatrixXf W2, Eigen::MatrixXf b2,Eigen::MatrixXf dW1,Eigen::MatrixXf db1, Eigen::MatrixXf dW2,Eigen::MatrixXf db2,float alpha);
 #endif
