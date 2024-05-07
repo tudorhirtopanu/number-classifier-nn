1f790414d9b17327e2233ef03864cc5327bc49fc
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-05-07 20:14:30 +0100

    add header for gradient descent func

--- include/neural_network.h
+++ include/neural_network.h
@@ -8,4 +8,7 @@ std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> f
 std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> backwardPropagation(Eigen::MatrixXf Z1, Eigen::MatrixXf A1, Eigen::MatrixXf Z2, Eigen::MatrixXf A2,Eigen::MatrixXf W1, Eigen::MatrixXf W2, Eigen::MatrixXf X, Eigen::VectorXi Y);
 
 std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> updateParameters(Eigen::MatrixXf W1,Eigen::MatrixXf b1,Eigen::MatrixXf W2, Eigen::MatrixXf b2,Eigen::MatrixXf dW1,Eigen::MatrixXf db1, Eigen::MatrixXf dW2,Eigen::MatrixXf db2,float alpha);
+
+std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> gradientDescent(Eigen::MatrixXf X,Eigen::VectorXi Y, float alpha, int iterations);
+
 #endif
