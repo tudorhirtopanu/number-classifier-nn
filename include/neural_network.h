a317351897807a56c5a8cb0bc913c8e49cd500c3
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-05-09 20:00:24 +0100

    add header for runImageThroughNetwork

--- include/neural_network.h
+++ include/neural_network.h
@@ -3,12 +3,14 @@
 
 #include <Eigen/Core>
 
-std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> forwardPropagation(Eigen::MatrixXf W1, Eigen::MatrixXf b1, Eigen::MatrixXf W2, Eigen::MatrixXf b2, Eigen::MatrixXf X);
+std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> forwardPropagation( Eigen::MatrixXf W1,Eigen::MatrixXf b1,Eigen::MatrixXf W2,Eigen::MatrixXf b2,Eigen::MatrixXf X);
 
 std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> backwardPropagation(Eigen::MatrixXf Z1, Eigen::MatrixXf A1, Eigen::MatrixXf Z2, Eigen::MatrixXf A2,Eigen::MatrixXf W1, Eigen::MatrixXf W2, Eigen::MatrixXf X, Eigen::VectorXi Y);
 
 std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> updateParameters(Eigen::MatrixXf W1,Eigen::MatrixXf b1,Eigen::MatrixXf W2, Eigen::MatrixXf b2,Eigen::MatrixXf dW1,Eigen::MatrixXf db1, Eigen::MatrixXf dW2,Eigen::MatrixXf db2,float alpha);
 
 std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> gradientDescent(Eigen::MatrixXf X,Eigen::VectorXi Y, float alpha, int iterations);
+
+Eigen::MatrixXf runImageThroughNetwork(const Eigen::MatrixXf& image, const Eigen::MatrixXf& W1, const Eigen::MatrixXf& b1, const Eigen::MatrixXf& W2, const Eigen::MatrixXf& b2);
 
 #endif
