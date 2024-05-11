469d07bbd4fe89c909e00173d16caeb2df3a0628
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-05-11 11:37:04 +0100

    add header for savePGM

--- include/dataset_utils.h
+++ include/dataset_utils.h
@@ -8,4 +8,6 @@ Eigen::MatrixXf readData(const std::string& filename);
 
 Eigen::VectorXi  readLabels(const std::string& filename);
 
+void savePGM(const std::string& filename, const Eigen::MatrixXf& image);
+
 #endif
