54031c3cb87b4be4cad3cb128b63e7580a8321f8
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2024-05-13 20:38:39 +0100

    update params for gradient descent header

--- include/neural_network.h
+++ include/neural_network.h
@@ -9,7 +9,7 @@ std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> b
 
 std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> updateParameters(Eigen::MatrixXf W1,Eigen::MatrixXf b1,Eigen::MatrixXf W2, Eigen::MatrixXf b2,Eigen::MatrixXf dW1,Eigen::MatrixXf db1, Eigen::MatrixXf dW2,Eigen::MatrixXf db2,float alpha);
 
-std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> gradientDescent(Eigen::MatrixXf X,Eigen::VectorXi Y, float alpha, int iterations);
+std::tuple<Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf, Eigen::MatrixXf> gradientDescent(Eigen::MatrixXf X,Eigen::VectorXi Y,Eigen::MatrixXf valX,Eigen::VectorXi valY, float alpha, int iterations);
 
 Eigen::MatrixXf runImageThroughNetwork(const Eigen::MatrixXf& image, const Eigen::MatrixXf& W1, const Eigen::MatrixXf& b1, const Eigen::MatrixXf& W2, const Eigen::MatrixXf& b2);
 
